0e96e76d7e073060c80783e4d1a81af108490213
Emmanuel <emmanuel_munoz9653@hotmail.com>
2018-07-12 22:00:07 -0500

    cambios

--- routes.js
+++ routes.js
@@ -1,25 +1,73 @@
 var express= require("express");
 var Zombie= require("./models/zombie");
 var Equipment = require("./models/equipment");
-
 var passport=require("passport");
+
+var acl = require("express-acl");
+
 var router= express.Router();
 
-router.use((req,res,next)=>{
-    res.locals.currentZombie=req.Zombie;
-    res.locals.errors=req.flash("error");
-    res.locals.infos=req.flash("info");
-    next();
+acl.config({
+    baseUrl: '/',
+    defaultRole:'invitado',     
 });
 
+router.use((req,res,next)=>{
+    res.locals.currentZombie=req.zombie;
+    res.locals.errors=req.flash("error");
+    res.locals.infos=req.flash("info");
+    if(req.zombie)
+    {
+        req.session.rol = req.zombie.rol;
+    }
 
-router.get("/signup", (req, res, next)=>{
+    if (req.session.rol == undefined){
+        acl.config({
+        baseUrl: '/',
+        defaultRole:'invitado',     
+    });
+    }
+
+    else
+    {
+        acl.config({
+            baseUrl: '/',
+            defaultRole:req.session.rol
+        });
+    }
+    next();
+});
+//zombie-king {ver y editar todo}
+//zombie-queen {ve todo}
+//zombie-boss {se encarga de la seccion de armas}
+//invitado {salir login y editar su perfil}
+
+router.use(acl.authorize);
+
+router.get("/login", (req, res)=>{
+    res.render("login");
+});
+
+router.post("/login", passport.authenticate("login", {
+    successRedirect:"/",
+    failureRedirect: "/login",
+    failureFlash: true
+}));
+
+router.get("/logout", (req, res) =>{
+    req.logout();
+    res.redirect("/");
+});
+
+//router.get("/signup", (req, res, next)=>{
+router.get("/signup", (req, res)=>{
     res.render("signup");
 });
 
 router.post("/signup", (req, res, next)=>{
     var username = req.body.username;
     var password = req.body.password;
+    var rol = req.body.rol;
 
     Zombie.findOne({username: username}, (err, zombie)=>{
         if(err){
@@ -31,12 +79,13 @@ router.post("/signup", (req, res, next)=>{
         }
             var newZombie = new Zombie({
                 username: username,
-                password: password
+                password: password,
+                rol: rol
             });
             newZombie.save(next);
             return res.redirect("/");
     });
-;})
+});
 
 router.get("/",(req,res,next)=>{
     Zombie.find()
@@ -55,7 +104,7 @@ router.get("/zombies/:username", (req, res, next)=>{
             return next(err);
         }
         if(!zombie){
-            return next(400);
+            return next(404);
         }
         res.render("profile", {zombie: zombie});
     });
@@ -96,6 +145,31 @@ router.get("/Equipment", (req, res, next)=>{
 
 });
 
+router.get("/edit", ensureAuthenticated, (req, res) => {
+    res.render("edit");
+})
+
+router.post("/edit", ensureAuthenticated, (req, res, next) => {
+    req.zombie.displayName = req.body.displayName;
+    req.zombie.bio = req.body.bio;
+    req.zombie.save((err) => {
+        if (err) {
+            next(err);
+            return;
+        }
+        req.flash("info", "Perfil actualizado!");
+        res.redirect("/edit");
+    });
+});
+
+function ensureAuthenticated(req, res, next) {
+    if(req.isAuthenticated()) {
+        next();
+    }else {
+        req.flash("info", "Necesitas iniciar sesion para poder ver esta seccion");
+        res.redirect("/login")
+    }
+}
+
 module.exports=router;
 
-///
