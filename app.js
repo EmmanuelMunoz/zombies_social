0cb0921d49960a0ee8df4c9290a2d40a273ecad6
Emmanuel <emmanuel_munoz9653@hotmail.com>
2018-07-12 21:56:40 -0500

    cambios

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var express= require("express");
+var mongoose= require("mongoose");
 
-var path = require("path");
-var bodyParser = require("body-parser");
-var cookieParser = require("cookie-parser");
-var session = require("express-session");
-var flash = require("connect-flash");
+var path= require("path");
+var bodyParser= require("body-parser");
+var cookieParser= require("cookie-parser");
+var flash= require("connect-flash");
+var session=require("express-session");
+var passport = require("passport");
 
-var routers = require("./routes");
-var app = express();
+var routes= require("./routes");
+var app= express();
+var passportsetup = require("./passportsetup");
 
 mongoose.connect("mongodb://localhost:27017/zombie_nest");
-app.set("port", process.env.PORT || 3000);
 
-app.set("views", path.resolve(__dirname, "views"));
-app.set("view engine", "ejs");
+passportsetup();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.set("port",process.env.PORT||3000);
+//direccion donde estan las rutas
+app.set("views",path.resolve(__dirname,"views"));
+app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(cookieParser());
 app.use(session({
-    secret: "marioeltiolocodelasalcanatrillas",
+    secret:"TKRvTKR",
     resave: true,
-    saveUninitialized: true
+    saveUninitialized:true
+
 }));
-
 app.use(flash());
-app.use(routers);
 
-app.listen(app.get("port"), () => {
-    console.log("La aplicacion se esta corriendo en  el puerto " + app.get("port"));
-});
+app.use(passport.initialize({
+    userProperty:"zombie"
+}));
+app.use(passport.session());
+
+app.use(routes);
+app.listen(app.get("port"),()=>{
+    console.log("la aplicacion inicio por el puerto"+app.get("port"));
+})
